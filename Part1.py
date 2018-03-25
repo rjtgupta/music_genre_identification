d0d544ade1a4f69b054538151b25300f5a15b4f3
Rajat Gupta <rjtgupta@uw.edu>
2018-03-25 09:49:43 -0700

    Adding files and documentation

---
+++ Part1.py
@@ -0,0 +1,120 @@
+
+# coding: utf-8
+
+import glob, os
+import scipy.io.wavfile
+from pydub import AudioSegment
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from mlxtend.plotting import plot_confusion_matrix
+
+artists = ['Adele', 'Eminem', 'Oasis']
+
+song_data = []
+sample_rate = []
+
+for artist in artists:
+    
+    files = 'Full_Songs/'+artist+'/*.mp3'
+    songs_list = glob.glob(files)
+    
+    for song in songs_list:
+        sound = AudioSegment.from_mp3(song)
+        
+        folder_names=song.split('/')
+        folder_mp3 = folder_names[0]+'/'+folder_names[1]+'/'+'Wav_files/'+folder_names[2]
+        wav_file = folder_mp3[:-4]+".wav"
+        sound.export(wav_file, format='wav')
+        
+        samplerate, data = scipy.io.wavfile.read(wav_file)
+        
+        #We are just keeping the 5 seconds into our song for the analysis
+        #We are keeping the 0th index (sound coming from left speaker- making audio mono instead of stereo)
+        for i in range(0,20):
+            song_data.append(data[5*i*samplerate:5*(i+1)*samplerate,0])
+        
+            sample_rate.append(samplerate)
+
+spec,_,_,_ = plt.specgram(song_data[0], Fs = sample_rate[0])
+rows, cols = spec.shape
+
+a = np.zeros(rows*cols, float)
+X = np.zeros((rows*cols, len(song_data)), float)
+
+for s in range (len(song_data)):
+    k = 0
+    spec,_,_,_ = plt.specgram(song_data[s], Fs = sample_rate[s])
+    for i in range (rows):
+        for j in range (cols):
+            a[k] = spec[i][j]
+            k += 1
+    X[:,s] = a
+
+U, sigma, V = np.linalg.svd(X, full_matrices=False)
+#As V returned by np.linalg.svd is actually V'
+#We will take do V =V.T to get its correct shape
+V = V.T
+
+length = []
+for i in range (sigma.shape[0]):
+    length.append(i)
+
+plt.semilogy(length, sigma/sum(sigma)) #s = sigma*0.0000005)
+
+plt.title('Singular Value Spectrum')
+plt.xlabel('mode number')
+plt.ylabel('Variance in mode')
+
+x1 = np.random.permutation(480)
+x2 = np.random.permutation(480)
+x3 = np.random.permutation(480)
+
+xpop = V[0:480,0:191]
+xrap = V[480:960,0:191]
+xrock= V[960:1440,0:191]
+
+xtrain = xpop[x1[:450],:]
+xtrain = np.append(xtrain, xrap[x2[:450],:],0)
+xtrain = np.append(xtrain, xrock[x3[:450],:],0)
+
+xtest = xpop[x1[450:],:]
+xtest = np.append(xtest, xrap[x2[450:],:],0)
+xtest = np.append(xtest, xrock[x3[450:],:],0)
+
+ytrain = []
+ytest = []
+for i in range(1350):
+    if(i<450):
+        ytrain.append('Adele')
+    elif(i>=450 and i<900):
+        ytrain.append('Eminem')
+    else:
+        ytrain.append('Oasis')
+        
+for i in range(90):
+    if(i<30):
+        ytest.append('Adele')
+    elif(i>=30 and i<60):
+        ytest.append('Eminem')
+    else:
+        ytest.append('Oasis')
+        
+pipeline = Pipeline((
+            ("scaler", StandardScaler()),
+            ("svm_clf", SVC(kernel='rbf', C=21, gamma = .00095) )
+            ))
+pipeline.fit(xtrain, ytrain)
+ypred_svm = pipeline.predict(xtest)
+
+accuracy_score(ytest, ypred_svm)
+
+conf_svm = confusion_matrix(ytest, ypred_svm)
+
+fig, ax = plot_confusion_matrix(conf_mat=conf_svm)
+plt.show()
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Music Genre Identification using Machine Learning
+Here, an analysis has been performed to identify music genre using 5 second audio clips using various machine learning algorithms on Python 3.6
+
+## Questions:
+We are attempting to answer the following questions:
+* Can the developed algorithm classify audio clips of bands belonging to different genres?
+* How accurately does the algorithm work when the same task has to be performed for bands within the same genre?
+* How well does the algorithm perform when we just want to predict the genre given a 5 second audio clip?
+
+## Files:
+* Github_report.pdf - Contains the documentation and explaination for the development of the algorithm.
+* Part1.py - Contains code for answering the 1st question.
+* Part2.py - Contains code for answering the 2nd question.
+* Part3.py - Contains code for answering the 3rd question.
